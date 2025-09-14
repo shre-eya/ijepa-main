3bbc97269641b01dd3e75af340236a794d11e5d0
shre-eya <shreeyak841@gmail.com>
2025-09-14 17:44:31 +0530

    Fix module import paths for src/masks and utils

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
+import sys
+import os
+
+# Add the 'src' directory to Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 import argparse
 
